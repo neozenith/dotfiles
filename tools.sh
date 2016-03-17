74b70c0844979b30ef47bf1c2380fb87c1d330dc
Josh Wilson <neozenith.dev@gmail.com>
2016-03-17 15:55:27 +1100

    Updated tools.sh

--- tools.sh
+++ tools.sh
@@ -2,13 +2,6 @@
 # Auth: Josh Wilson
 # Desc: Install script for associated syntax checker tools 
 
-# Vundle
-if [ ! -d ".vim/bundle/Vundle.vim/.git" ]; then
-  git submodule add https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
-else
-  git submodule update
-fi
-
 #HomeBrew
 brew install npm
 
@@ -20,3 +13,12 @@ gem install rubocop
 #JavaScript
 npm install -g jshint
 npm install -g jscs
+
+# Vundle
+if [ ! -d ".vim/bundle/Vundle.vim/.git" ]; then
+  git submodule add https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
+else
+  git submodule update
+fi
+# Install Plugins
+vim +PluginInstall +qall
