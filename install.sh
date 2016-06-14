6d4cc8706ec30c09e83e4d22b461df9312c34b85
Josh Wilson <neozenith.dev@gmail.com>
2016-06-14 15:32:42 +1000

    Added link to blog that gets Vim mostly working on windows

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 # Auth: Josh Wilson
 # Desc: Install script for associated syntax checker tools 
 
+# Installation on Windows
+# https://medium.com/@saaguero/setting-up-vim-in-windows-5401b1d58537#.a3huqnkx7
+
+
 clear
 
 function confirm () {
