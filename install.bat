967633d11e1f275e08de7528f4bcf3a91d486610
Josh Peak <neozenith.dev@gmail.com>
2017-01-16 13:00:21 +1100

    Set install script to just copy instead of symlink for now

--- install.bat
+++ install.bat
@@ -1,11 +1,20 @@
-#! /usr/bin/bash
-# Auth: Josh Wilson
-# Desc: Install script for associated syntax checker tools
+@echo off
+:: Auth: Josh Peak
+:: Desc: Install script for associated syntax checker tools
 
-# Installation on Windows
-# https://medium.com/@saaguero/setting-up-vim-in-windows-5401b1d58537#.a3huqnkx7
+:: Installation on Windows
+:: https://medium.com/@saaguero/setting-up-vim-in-windows-5401b1d58537#.a3huqnkx7
 
 
 cls
 
+rm -rfv %HOME%\_vimrc
+rm -rfv %HOME%\vimfiles
 
+cp -v _vimrc %HOME%\_vimrc
+xcopy /s .vim %HOME%\vimfiles
+
+:: https://github.com/VundleVim/Vundle.vim/wiki/Vundle-for-Windows
+git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/vimfiles/bundle/Vundle.vim
+
+vim +PluginInstall +qall
