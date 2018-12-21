1d242ab2749a84765dfc3fdcc90819222810ec1a
Josh Peak <neozenith.dev@gmail.com>
2018-12-21 18:18:13 +1100

    update docker pi install script

--- rpi-scripts/install-docker.sh
+++ rpi-scripts/install-docker.sh
@@ -1,3 +1,5 @@
 # https://medium.freecodecamp.org/the-easy-way-to-set-up-docker-on-a-raspberry-pi-7d24ced073ef
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker `whoami`
+systemctl start docker
 
