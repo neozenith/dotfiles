fe9c5e79e481850b7e1c8e9214a7727d642361c7
Josh Peak <neozenith.dev@gmail.com>
2018-12-29 17:34:11 +1100

    Update rpi node installer to actually work and annotate fixes to come

--- rpi-scripts/install-node.sh
+++ rpi-scripts/install-node.sh
@@ -2,13 +2,29 @@
 export ARCH=`uname -m`
 # TODO: Get latest version by curl scraping this URL
 # https://nodejs.org/en/download/current/
-export NODE_VERSION=`curl -s https://nodejs.org/en/download/current/ | grep -E 'v[0-9][0-9]' | sed -r s/.*\(v[0-9]+\.[0-9]+.[0-9]+\).*/\\1/g | head -1`
+
+
+NODE_DL=`curl -s https://nodejs.org/en/download/current/ | grep -E 'v[0-9][0-9]' | grep "$ARCH"`
+
+echo $NODE_DL
+
+# TODO: get sed regex working consistently across platforms
+export NODE_VERSION=`curl -s https://nodejs.org/en/download/current/ | grep -E 'v[0-9][0-9]' | grep "$ARCH" | sed -r s/.*\(v[0-9]+\.[0-9]+.[0-9]+\).*/\\1/g | head -1`
+
+# just hardcode latest version
+export NODE_VERSION=v11.6.0
+
 export NODE_DIST="node-$NODE_VERSION-linux-$ARCH"
+echo $NODE_VERSION
+echo $NODE_DIST
 cd $HOME
 wget "https://nodejs.org/dist/$NODE_VERSION/$NODE_DIST.tar.xz"
-tar -xvf "$NODE_DIST.tar.xz" 
-cd "$HOME/$NODE_DIST"
-[ -n "$(pwd | grep node 2> /dev/null)" ] && sudo cp -Rv * /usr/local
+  if [[ -e "$NODE_DIST.tar.xz" ]]; then
+  tar -xvf "$NODE_DIST.tar.xz" 
+  cd "$HOME/$NODE_DIST"
+  [ -n "$(pwd | grep node 2> /dev/null)" ] && sudo cp -Rv * /usr/local
+fi
+
 which node
 node --version
 which npm
