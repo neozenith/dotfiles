553a384834b58c1082cc066389cd08f9d19a5d36
Josh Peak <neozenith.dev@gmail.com>
2020-12-21 11:29:02 +1100

    Improve k8s prompt formatting

--- bash-scripts/function_parse_kubectl_prompt.sh
+++ bash-scripts/function_parse_kubectl_prompt.sh
@@ -4,16 +4,17 @@ parse_k8s_prompt() {
     [[ $OSTYPE == darwin* ]] && ESC_CODE="\033"
 
     local DARK="$ESC_CODE[2;49;94m"
-    local BRIGHT="$ESC_CODE[7;49;94m"
+    local BRIGHT="$ESC_CODE[1;49;94m"
+    local BRIGHTEST="$ESC_CODE[7;49;94m"
     local NORM="$ESC_CODE[0m"
     local K8S_CONTEXT=`kubectl config current-context 2> /dev/null`
     local K8S_PROMPT=""
-    local K8S_COLOUR="${BRIGHT}"
+    local K8S_COLOUR="${BRIGHTEST}"
 
     if [ -n "$K8S_CONTEXT" ]; then
       [[ "$K8S_CONTEXT" = "docker-desktop" ]] && K8S_COLOUR="${DARK}"
 
-      K8S_PROMPT="${K8S_COLOUR}[K8S:${K8S_CONTEXT}]${NORM}"
+      K8S_PROMPT="${BRIGHT}☸|${K8S_COLOUR}${K8S_CONTEXT}${NORM}${BRIGHT}|${NORM}"
     fi
     echo -e "${K8S_PROMPT}"
 }
