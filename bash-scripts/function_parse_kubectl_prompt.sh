da943b82417ebca1e642ef972330efe04fa1c399
Josh Peak <neozenith.dev@gmail.com>
2020-12-21 11:12:55 +1100

    Colour K8s prompt to be bright for contexts that are not docker-desktop

--- bash-scripts/function_parse_kubectl_prompt.sh
+++ bash-scripts/function_parse_kubectl_prompt.sh
@@ -3,9 +3,18 @@ parse_k8s_prompt() {
     local ESC_CODE="\e"
     [[ $OSTYPE == darwin* ]] && ESC_CODE="\033"
 
-    local PURPLE="$ESC_CODE[2;49;94m"
+    local DARK="$ESC_CODE[2;49;94m"
+    local BRIGHT="$ESC_CODE[7;49;94m"
     local NORM="$ESC_CODE[0m"
-  local K8S_CONTEXT=`kubectl config current-context 2> /dev/null`
-  echo -e "\n${PURPLE}${K8S_CONTEXT}${NORM}"
+    local K8S_CONTEXT=`kubectl config current-context 2> /dev/null`
+    local K8S_PROMPT=""
+    local K8S_COLOUR="${BRIGHT}"
+
+    if [ -n "$K8S_CONTEXT" ]; then
+      [[ "$K8S_CONTEXT" = "docker-desktop" ]] && K8S_COLOUR="${DARK}"
+
+      K8S_PROMPT="${K8S_COLOUR}[K8S:${K8S_CONTEXT}]${NORM}"
+    fi
+    echo -e "${K8S_PROMPT}"
 }
 export -f parse_k8s_prompt
