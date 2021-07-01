9184bc693e9b95369ff181bd24f9bda3e8b83ee9
Josh Peak <neozenith.dev@gmail.com>
2021-07-01 20:20:02 +1000

    Polished python prompt

--- zsh/scripts/function_parse_python_prompt.sh
+++ zsh/scripts/function_parse_python_prompt.sh
@@ -1,3 +1,18 @@
 function parse_python_prompt() {
-  echo -e "\n$(python3 -V) $(which python3)"
+  CURRENT_PY="$(which python3)"
+  PY_LOCATION=""
+
+  if [[ $CURRENT_PY == "/usr/bin/python3" ]]; then
+    PY_LOCATION="system"
+  elif [[ $CURRENT_PY == "/usr/local/bin/python3" ]]; then
+    PY_LOCATION="homebrew"
+  elif [[ $CURRENT_PY == "$HOME/.pyenv/shims/python3" ]]; then
+    PY_LOCATION="pyenv"
+  else
+    if [[ -n $VIRTUAL_ENV ]]; then
+      PY_LOCATION="venv"
+    fi
+  fi
+
+  echo -e " %F{green}🐍 $PY_LOCATION $(python3 -V | sed 's/Python //g')%F{rc}"
 }
