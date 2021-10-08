0f458a04367424e3e3d02aacd009c1e9a44ac4f2
Josh Peak <neozenith.dev@gmail.com>
2021-10-08 22:00:34 +1100

    Add terraform aliases

--- zsh/scripts/aliases_common.sh
+++ zsh/scripts/aliases_common.sh
@@ -4,4 +4,7 @@ alias cdp="cd ~/play"
 alias cdw="cd ~/work"
 alias cdd="cd ~/dotfiles"
 alias awswho="aws sts get-caller-identity | jq .Arn"
+alias tfa="terraform apply -auto-approve"
+alias tfd="terraform destroy -auto-approve"
+alias tff="terraform fmt && terraform validate && terraform graph | dot -Tsvg > graph.svg"
 
