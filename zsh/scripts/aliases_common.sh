721a60ec476ebb467017ec34ded030df70f4cd98
Josh Peak <neozenith.dev@gmail.com>
2021-10-28 12:57:19 +1100

    Add alias for quickly launching a python virtual env

--- zsh/scripts/aliases_common.sh
+++ zsh/scripts/aliases_common.sh
@@ -3,8 +3,12 @@ alias ll="ls -laGHh"
 alias cdp="cd ~/play"
 alias cdw="cd ~/work"
 alias cdd="cd ~/dotfiles"
+
 alias awswho="aws sts get-caller-identity | jq .Arn"
+
 alias tfa="terraform apply -auto-approve"
 alias tfd="terraform destroy -auto-approve"
 alias tff="terraform fmt && terraform validate && terraform graph | dot -Tsvg > graph.svg"
+
+alias pvup="python3 -m venv .venv && .venv/bin/python3 -m pip install -U pip -r requirements.txt && . ./.venv/bin/activate"
 
