1b0e3e94b47386d8c789d2154a4bf406c72f344c
Josh Peak <neozenith.dev@gmail.com>
2024-10-16 14:51:15 +1100

    wip: aws sso login and export alias automation

--- zsh/scripts/aliases_common.sh
+++ zsh/scripts/aliases_common.sh
@@ -5,10 +5,10 @@ alias cdw="cd ~/work"
 alias cdd="cd ~/dotfiles"
 
 alias awswho="aws sts get-caller-identity | jq .Arn"
+# alias awssso="aws sso login && eval \"$$(aws configure export-credentials --profile $$AWS_PROFILE --format env)\""
 
 alias tfa="terraform apply -auto-approve"
 alias tfd="terraform destroy -auto-approve"
 alias tff="terraform fmt && terraform validate && terraform graph | dot -Tsvg > graph.svg"
 
 alias pvup="python3 -m venv .venv && .venv/bin/python3 -m pip install --upgrade pip && .venv/bin/python3 -m pip install -r requirements.txt && . ./.venv/bin/activate"
-
